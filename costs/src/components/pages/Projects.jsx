12e80bba386b979c5fd9b77dc331798419fff5e7
kiosalvatore <cariocacaiogomes@gmail.com>
2024-04-23 17:12:20 -0300

    commit page projects

--- costs/src/components/pages/Projects.jsx
+++ costs/src/components/pages/Projects.jsx
@@ -1,6 +1,8 @@
 import Message from '../layout/Message'
 import { useNavigate } from 'react-router-dom'
-
+import styles from './Projects.module.css'
+import Container from '../layout/Container'
+import LinkButton from '../layout/LinkButton'
 
 export default function Projects() {
 
@@ -12,9 +14,17 @@ export default function Projects() {
   
 
   return (
-    <div>
-      <h1>Meus Projetos</h1>
+    <div className={styles.project_container}>
+      <div className={styles.title_container}>
+        <h1>Meus Projetos</h1>
+        <LinkButton to="/newproject" text="Criar projeto"/>
+      </div>
+
       {message && <Message type="success" msg={message} />}
+
+      <Container customClass="start">
+          <p>Projetos...</p>
+      </Container>
     </div>
   )
 }
